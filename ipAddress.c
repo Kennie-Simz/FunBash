a974e474d1fc56408dd4f11ba79762fef4b89da9
Kennie-Simz <nyongesa17@outlook.com>
2022-05-19 00:36:44 -0700

    getting my IP

---
+++ ipAddress.c
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Creating your own commands in terminal. This one puts the echo command into one word 'ipAddress' given as an alias variable
+# and makes it easier to get your ip Address using the terminal
+# Usage: Type the following in terminal and then press return or enter
+# Then type the given name 'myIPAddress' in terminal as is to get your IP Address
+
+alias myIP="echo $(ifconfig | grep broadcast | awk '{print $2}')"
